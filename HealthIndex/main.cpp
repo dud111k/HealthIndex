2ed05cc0f0217b1093135effbdafc5d2b341ef44
dud111k <hermorzhoviy36@gmail.com>
2025-05-22 01:34:44 +0300

    Update main.cpp

--- HealthIndex/main.cpp
+++ HealthIndex/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("HealthIndex");
+    w.resize(300,200);
     w.show();
     return a.exec();
 }
